0c671bf79dbddc8c3f858154723d3aa418046ed1
10000000-pbl2018 <sotuken_pbl1@yahoo.co.jp>
2018-09-27 21:06:13 +0900

    10回目

--- src/pbl_gp1/Pbl_gp1.java
+++ src/pbl_gp1/Pbl_gp1.java
@@ -15,7 +15,7 @@ public class Pbl_gp1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("fjlgkfds");
+        System.out.println("646846515");
     }
     
 }
