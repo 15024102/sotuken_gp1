1347b3f818c916bf9376a0f8736d97a851e172ac
15024102 <15024102@mmm.muroran-it.ac.jp>
2018-09-27 20:58:12 +0900

    1回目

---
+++ src/pbl_gp1/Pbl_gp1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbl_gp1;
+
+/**
+ *
+ * @author 祐一
+ */
+public class Pbl_gp1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("こんにちは");
+    }
+    
+}
